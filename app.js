601d1256c498777ec68e7b3c9b12880b019d4110
Jamie Smith <me@jsmith.dev>
2023-01-22 14:35:25 -0500

    clean up

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-const dotenv = require('dotenv');
 const express = require('express');
 //const cors = require('cors');
 //const helmet = require('helmet');
 const fetch = require('node-fetch');
 
-
-if(!process.env.NODE_ENV || process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test' ){
-    dotenv.config()
-}
-
 const app = express()
 const port = process.env.PORT || 4242;
-const jsonParser = express.json();
 
 //app.use(helmet());
 
@@ -21,14 +14,14 @@ const jsonParser = express.json();
 
 app.get('/', async (req, res) => {
 
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
-    //res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET");
-    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-
     if (req.method === 'OPTIONS') {
+
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET");
+        res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+
         // CORS Preflight
-        res.status(200).send();
+        res.send().status(200);
     }
     else {
 
@@ -36,8 +29,7 @@ app.get('/', async (req, res) => {
             
             if(!req.query.url) return res.status(400).send('A url parameter is required');
             
-            res.status(200).send(await (await fetch(req.query.url)).text());
-            
+            res.send(await (await fetch(req.query.url)).text()).status(200);
         }
         catch(error){
             console.error( error )
@@ -45,38 +37,9 @@ app.get('/', async (req, res) => {
         }
     }
 })
-
-app.post('/', jsonParser, async (req, res) => {
-
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "POST");
-    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-
-    if (req.method === 'OPTIONS') {
-        // CORS Preflight
-        res.status(200).send();
-    }
-    else {
-
-        try {
-            
-            const { url } = req.body;
-
-            if(!url) return res.status(400).send('A url parameter is required');
-            
-            return res.send(await (await fetch(url)).text()).status(200);
-            
-        }
-        catch(error){
-            console.error( error )
-            res.status(400).send(error.message)
-        }
-    }
-})
-
 
 app.listen(port, () => console.log(`Initialized. Using port ${port} - ${new Date().toUTCString()}`))
+
 
 /**
  * @description For AWS Lambda, export app 
