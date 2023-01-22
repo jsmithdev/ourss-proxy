8ab1f787ee634391ad393709b1fb43c99a6cd578
Jamie Smith <me@jsmith.dev>
2023-01-22 17:02:03 -0500

    convert to stream; rm unneeded

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-const app = require('./app')
+const express = require('express');
+//const cors = require('cors');
+//const helmet = require('helmet');
+const fetch = require('node-fetch');
 
-const serverless = require('serverless-http')
+const app = express()
+const port = process.env.PORT || 4242;
 
-const handler = serverless(app)
+//app.use(helmet());
 
+//app.use(cors({
+//    origin: process.env.SF_URL
+//}))
 
-module.exports.handler = async (event, context) => {
+app.get('/', async (req, res) => {
+    
+    console.time('request')
 
-    return await handler(event, context)
-}
+    if (req.method === 'OPTIONS') {
+
+        // CORS Preflight
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET");
+        res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+        res.status(200).send();
+    }
+    else {
+
+        try {
+            
+            if(!req.query.url) return res.status(400).send('A url parameter is required');
+            
+            const prox = await fetch(req.query.url)
+
+            prox.body.pipe(res);
+        }
+        catch(error){
+            console.error( error )
+            res.status(400).send(error.message)
+        }
+    }
+
+    console.timeEnd('request')
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+})
+
+app.listen(port, () => console.log(`Initialized. Using port ${port} - ${new Date().toUTCString()}`))
+
+/**
+ * @description For AWS Lambda, export app 
+ */
+module.exports = app;
